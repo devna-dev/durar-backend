bff63f26a6deb545e6678c260bf9623b193b9fbe
Ahmed <adhm_n4@yahoo.com>
2020-07-03 19:41:40 +0200

    Add sub categories list to category API

--- alshamelah_api/apps/categories/serializers.py
+++ alshamelah_api/apps/categories/serializers.py
@@ -3,13 +3,16 @@ from rest_framework import serializers
 from .models import Category, SubCategory
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name', ]
-
-
 class SubCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategory
         fields = ['id', 'name', 'category']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    sub_categories = SubCategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'sub_categories']
+        read_only_fields = ['sub_categories']
